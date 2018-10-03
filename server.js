c1b7102337898b8f8d02f5e887f645b5ee4af10d
Pankajtanwarbanna <2016ucp1381@mnit.ac.in>
2018-10-03 19:25:40 +0530

    Server setup

---
+++ server.js
@@ -0,0 +1,25 @@
+var express  = require('express');
+var app = express();
+var morgan = require('morgan');             // middleware to log http requests
+var port = 8080 || process.env.PORT;
+var mongoose = require('mongoose');
+
+app.use(morgan('dev'));
+
+// connecting to mongo database
+mongoose.connect('mongodb://localhost:27017/zulu', { useNewUrlParser: true }, function (err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Successfully connected to database.');
+    }
+});
+
+app.get('/', function (req,res) {
+    res.send('Hello from Zulu.')
+});
+
+// server listening on port 8080
+app.listen(port, function () {
+    console.log('Server running on port 8080');
+});
