90bae0f7ae3c23343f1dbabd71a81f5bcb0a2b9d
Pankajtanwarbanna <2016ucp1381@mnit.ac.in>
2018-10-06 22:17:43 +0530

    Email controller added

--- public/app/app.js
+++ public/app/app.js
@@ -1,4 +1,4 @@
-angular.module('userApp', ['userRoutes','userCtrl','mainController','managementController'])
+angular.module('userApp', ['userRoutes','userCtrl','mainController','managementController','emailController'])
 
 .config(function ($httpProvider) {
     $httpProvider.interceptors.push('AuthInterceptors');
